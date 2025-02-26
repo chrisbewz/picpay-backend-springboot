339d373c3a2d46090229ab60a6c3f9996b5d37fe
christianb <christianb@weg.net>
2025-02-25 23:40:09 -0300

    Refactor: - Remove extra unused imports - Remove mongo db related annotations

--- src/main/java/br/com/picpay/backend/PicpayBackendApplication.java
+++ src/main/java/br/com/picpay/backend/PicpayBackendApplication.java
@@ -1,14 +1,9 @@
 package br.com.picpay.backend;
 
-import br.com.picpay.backend.data.mongo.repositories.UserRepository;
-import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongock
-@EnableMongoRepositories(basePackageClasses = UserRepository.class)
 public class PicpayBackendApplication {
 
     public static void main(String[] args) {
