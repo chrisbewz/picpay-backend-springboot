1fd52e737f52782e6a456002b67fd01aa4789d93
christianb <christianb@weg.net>
2025-03-02 09:21:09 -0300

    Docs: - Fix naming conventions on test classes

--- src/test/java/br/com/picpay/backend/PicpayBackendChallengeApplicationTests.java
+++ src/test/java/br/com/picpay/backend/PicpayBackendChallengeApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.picpay.backend;
 import br.com.picpay.backend.config.TestContainersConfiguration;
 import org.springframework.boot.SpringApplication;
 
-public class TestPicpayDesafioBackendApplication {
+public class PicpayBackendChallengeApplicationTests {
 
     public static void main(String[] args) {
         SpringApplication.from(PicpayBackendApplication::main).with(TestContainersConfiguration.class).run(args);
